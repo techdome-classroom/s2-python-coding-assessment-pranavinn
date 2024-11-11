b5f78c2eb4895079797f1fd631685a822d810fe5
pranavinn <128679594+pranavinn@users.noreply.github.com>
2024-11-11 15:11:53 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,9 +1,33 @@
 class Solution(object):
     def romanToInt(self, s):
+        
         """
         :type s: str
         :rtype: int
         """
+        roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+        res = 0
+        prev= 0
+        for char in reversed(s):
+            cur = roman_values[char]
+            if cur < prev:
+                res -= cur
+            else:
+                res += cur
+             prev = cur
+    
+    return res
+
+
         pass
 
 
